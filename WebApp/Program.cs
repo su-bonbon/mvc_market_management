13762e5567c60846a56973d02ab1fd980e9722b1
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-16 14:47:54 -0800

    begin MVC framework

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -4,22 +4,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", (HttpContext context) => {
-    string html = @"<html>
-                        <body>
-                            <h1>Hello World!</h1>
-                            <br>
-                            Welcome to this new world!
-                        </body>
-                    <html>";
-    WriteHtml(context, html);
-});
+
 
 app.Run();
 
-void WriteHtml(HttpContext context, string html)
-{
-    context.Response.ContentType = MediaTypeNames.Text.Html;
-    context.Response.ContentLength = Encoding.UTF8.GetByteCount(html);
-    context.Response.WriteAsync(html);
-}
