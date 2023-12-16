471445f3aaa0805a7f7a8de0e98ba7088361ece3
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-16 14:40:18 -0800

    create write html method

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,25 @@
+using System.Net.Mime;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (HttpContext context) => {
+    string html = @"<html>
+                        <body>
+                            <h1>Hello World!</h1>
+                            <br>
+                            Welcome to this new world!
+                        </body>
+                    <html>";
+    WriteHtml(context, html);
+});
 
 app.Run();
+
+void WriteHtml(HttpContext context, string html)
+{
+    context.Response.ContentType = MediaTypeNames.Text.Html;
+    context.Response.ContentLength = Encoding.UTF8.GetByteCount(html);
+    context.Response.WriteAsync(html);
+}
