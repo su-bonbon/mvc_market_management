a90c5e1d9f63aa9c940e9e78dd2a3e6a437a62b2
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-19 20:17:10 -0800

    test navbar - passed

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0769a4e2e804d5178f216d084215ce0facfeebd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5262c30b4b1d20a917346edae1d0405d39bd36")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
