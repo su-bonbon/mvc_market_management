87a58049e78375379ab83b119ba3b81e5d79352a
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-21 11:57:09 -0800

    understand model binding

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -11,7 +11,7 @@ namespace WebApp.Controllers
             return View(categories);
         }
 
-        public IActionResult Edit(int? id)
+        public IActionResult Edit([FromRoute]int? id)
         {
             var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
             return View(category);
