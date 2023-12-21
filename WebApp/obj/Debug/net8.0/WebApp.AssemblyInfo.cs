8a4022deed3144554dfa1f82161142b99f086d86
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-21 13:37:34 -0800

    add model binding format (noticed view import is not working)

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a58049e78375379ab83b119ba3b81e5d79352a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091d55123d5760954434d7c1555af41021f4ff6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
