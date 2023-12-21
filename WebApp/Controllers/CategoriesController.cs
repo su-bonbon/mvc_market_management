32693de9911d479a2959409f351061c00046e18a
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-21 15:47:21 -0800

    add data validation

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -21,8 +21,12 @@ namespace WebApp.Controllers
         [HttpPost]
         public IActionResult Edit(Category category)
         {
-            CategoriesRepository.UpdateCategory(category.CategoryId, category);
-            return RedirectToAction(nameof(Index));
+            if (ModelState.IsValid)
+            {
+				CategoriesRepository.UpdateCategory(category.CategoryId, category);
+				return RedirectToAction(nameof(Index));
+			}
+           return View(category);
         }
     }
 }
--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b310c4a8b7df02e47644572c48e1f9a33b16b3dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da3dcf797f11dce93ac7be3f579e13901119e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
