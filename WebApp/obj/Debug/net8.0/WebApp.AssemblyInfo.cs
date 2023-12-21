37f72a4f83d82a7ce2868fdb1ba58c389adfa952
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-21 15:53:35 -0800

    send error msg when user delete the name of the category

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da3dcf797f11dce93ac7be3f579e13901119e72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32693de9911d479a2959409f351061c00046e18a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
