6b5da4e0a31bc4cd61619a7c4cb9453f165e1da7
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-21 16:05:00 -0800

    create a button

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f72a4f83d82a7ce2868fdb1ba58c389adfa952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+672be3112b61d545af79d422ddd8816f35ecb134")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
