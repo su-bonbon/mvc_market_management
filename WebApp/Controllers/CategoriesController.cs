4c90464c723f8bdb101a6fbb8c5ea2e1b474c7d0
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-21 18:14:14 -0800

    create htttp post action method

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -46,5 +46,10 @@ namespace WebApp.Controllers
 			return View(category);
 		}
 
+        public IActionResult Delete(int categoryId)
+        {
+            CategoriesRepository.DeleteCategory(categoryId);
+            return RedirectToAction(nameof(Index));
+        }
 	}
 }
