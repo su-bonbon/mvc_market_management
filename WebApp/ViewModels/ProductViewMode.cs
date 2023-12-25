ea00a1792797bd7ee8a0f3ff091d11e5ed208d39
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-24 23:29:20 -0600

    implement productview model

---
+++ WebApp/ViewModels/ProductViewMode.cs
@@ -0,0 +1,10 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class ProductViewMode
+    {
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+        public Product Product { get; set; } = new Product();
+    }
+}
