63566508131a7de0494d1206d0f71a709e319e4b
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-24 23:34:04 -0600

    create product add view

--- WebApp/Controllers/ProductsController.cs
+++ WebApp/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
@@ -9,6 +10,16 @@ namespace WebApp.Controllers
 		{
 			var products = ProductsRepository.GetProducts(loadCategory: true);
 			return View(products);
+		}
+
+		public IActionResult Add()
+		{
+			var productViewModel = new ProductViewModel
+			{
+				Categories = CategoriesRepository.GetCategories()
+			};
+
+			return View(productViewModel);
 		}
 	}
 }
--- WebApp/ViewModels/ProductViewModel.cs
+++ WebApp/ViewModels/ProductViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace WebApp.ViewModels
 {
-    public class ProductViewMode
+    public class ProductViewModel
     {
         public IEnumerable<Category> Categories { get; set; } = new List<Category>();
         public Product Product { get; set; } = new Product();
