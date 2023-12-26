0e5052a8578fee38eebd523aab81dd98bb8b6b38
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-25 22:15:21 -0600

    test - passed

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a04c0d1aa57166f0218524749a37cb83971dce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607d21710e22bcf9d2fe87f3118acc0c6e78f0e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
