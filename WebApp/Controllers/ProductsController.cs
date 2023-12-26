f9ac03c00be66f74aa599e15755c2cd38eb7f10a
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-25 19:37:30 -0600

    create product edit action method

--- WebApp/Controllers/ProductsController.cs
+++ WebApp/Controllers/ProductsController.cs
@@ -34,5 +34,16 @@ namespace WebApp.Controllers
 			productViewModel.Categories = CategoriesRepository.GetCategories();
 			return View(productViewModel);
 		}
+
+		public IActionResult Edit(int id) 
+		{
+			var productViewModel = new ProductViewModel
+			{
+				Product = ProductsRepository.GetProductById(id) ?? new Product(),
+				Categories = CategoriesRepository.GetCategories()
+			};
+
+			return View(productViewModel);
+		}
 	}
 }
