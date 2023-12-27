50fd7b91540ee4be98bab947d34ad8bb502e8ea6
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-26 20:00:04 -0600

    modify sales view model

--- WebApp/ViewModels/SalesViewModel.cs
+++ WebApp/ViewModels/SalesViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace WebApp.ViewModels
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels
 {
 	public class SalesViewModel
 	{
+		public int SelectedCategoryId { get; set; }
+		public IEnumerable<Category> Categories { get; set; } = new List<Category>();
 	}
 }
