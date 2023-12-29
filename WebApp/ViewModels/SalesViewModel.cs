116cab44a73ccf8e69f5891af987a1cafc2d7b63
Sujin <108903065+su-bonbon@users.noreply.github.com>
2023-12-29 17:56:35 -0600

    modify sales view model to add quantity

--- WebApp/ViewModels/SalesViewModel.cs
+++ WebApp/ViewModels/SalesViewModel.cs
@@ -6,5 +6,9 @@ namespace WebApp.ViewModels
 	{
 		public int SelectedCategoryId { get; set; }
 		public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+
+		public int SelectedProductId { get; set; }
+
+		public int QuantityToSell { get; set; }
 	}
 }
