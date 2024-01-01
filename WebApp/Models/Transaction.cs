e537d1e2919a8f601432d6b530fde6de7a99e9c9
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-01 14:06:46 -0600

    create contents of the transaction model

--- WebApp/Models/Transaction.cs
+++ WebApp/Models/Transaction.cs
@@ -2,5 +2,13 @@
 {
 	public class Transaction
 	{
+		public int TransactionId { get; set; }
+		public DateTime TimeStamp { get; set; }
+		public int ProductId { get; set; }
+		public string ProductName { get; set; } = "";
+		public double Price { get; set; }
+		public int BeforeQty {  get; set; }
+		public int SoldQty { get; set; }
+		public string CashierName { get; set; } = "";
 	}
 }
