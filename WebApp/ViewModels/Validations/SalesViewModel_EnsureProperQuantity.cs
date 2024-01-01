2d5a9579a9ad22c17f7aec468a92cae683b7c999
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-01 13:31:01 -0600

    modify the contents of the validation class

--- WebApp/ViewModels/Validations/SalesViewModel_EnsureProperQuantity.cs
+++ WebApp/ViewModels/Validations/SalesViewModel_EnsureProperQuantity.cs
@@ -1,6 +1,22 @@
-﻿namespace WebApp.ViewModels.Validations
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels.Validations
 {
-	public class SalesViewModel_EnsureProperQuantity
+	public class SalesViewModel_EnsureProperQuantity : ValidationAttribute
 	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			var salesViewModel = validationContext.ObjectInstance as SalesViewModel;
+			if (salesViewModel != null)
+			{
+				if(salesViewModel.QuantityToSell <= 0)
+				{
+					return new ValidationResult("The quantity to sell has to be greater than zero");
+				}
+			}
+			return ValidationResult.Success;
+		}
+		
+
 	}
 }
