0de140001f14292fd4a73b026d0ee0529b76121d
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-01 14:42:47 -0600

    implement transaction repository

--- WebApp/Models/Transaction.cs
+++ WebApp/Models/Transaction.cs
@@ -5,7 +5,7 @@
 		public int TransactionId { get; set; }
 		public DateTime TimeStamp { get; set; }
 		public int ProductId { get; set; }
-		public string ProductName { get; set; } = "";
+		public string ProductName { get; set; } = "";  // in case product name changes
 		public double Price { get; set; }
 		public int BeforeQty {  get; set; }
 		public int SoldQty { get; set; }
---
+++ WebApp/Models/TransactionsRepository.cs
@@ -0,0 +1,52 @@
+﻿namespace WebApp.Models
+{
+	public class TransactionsRepository
+	{
+		private static List<Transaction> transactions = new List<Transaction>();
+		public static IEnumerable<Transaction> GetByDayAndCashier(string cashierName, DateTime date)
+		{
+			if (string.IsNullOrWhiteSpace(cashierName))
+				return transactions.Where(x => x.TimeStamp.Date == date.Date);
+			else
+				return transactions.Where(x =>
+					x.CashierName.ToLower().Contains(cashierName.ToLower()) &&
+					x.TimeStamp.Date == date.Date);
+		}
+
+		public static IEnumerable<Transaction> Search(string cashierName, DateTime startDate, DateTime endDate)
+		{
+			if (string.IsNullOrWhiteSpace(cashierName))
+				return transactions.Where(x => x.TimeStamp >= startDate.Date && x.TimeStamp <= endDate.Date.AddDays(1));
+			else
+				return transactions.Where(x =>
+				x.CashierName.ToLower().Contains(cashierName.ToLower()) &&
+				x.TimeStamp >= startDate.Date && x.TimeStamp <= endDate.Date.AddDays(1).Date);
+		}
+
+		public static void Add(string cashierName, int productId, string productName, double price, int beforeQty, int soldQty)
+		{
+			var transaction = new Transaction
+			{
+				ProductId = productId,
+				ProductName = productName,
+				TimeStamp = DateTime.Now,
+				Price = price,
+				BeforeQty = beforeQty,
+				SoldQty = soldQty,
+				CashierName = cashierName,
+			};
+
+			if (transaction != null && transactions.Count > 0)
+			{
+				var maxId = transactions.Max(x => x.TransactionId);
+				transaction.TransactionId = maxId + 1;
+			}
+			else
+			{
+				transaction.TransactionId = 1;
+			}
+
+			transactions?.Add(transaction);
+		}
+	}
+}
