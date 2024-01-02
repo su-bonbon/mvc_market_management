424f8734eadefb3c0a66dd22e8033d508825ea05
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-02 16:16:56 -0600

    modify transaction controller

---
+++ WebApp/Controllers/TransactionsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+	public class TransactionsController : Controller
+	{
+		public IActionResult Index()
+		{
+			TransactionsViewModel transactionsViewModel = new TransactionsViewModel();
+			return View(transactionsViewModel);
+		}
+	}
+}
