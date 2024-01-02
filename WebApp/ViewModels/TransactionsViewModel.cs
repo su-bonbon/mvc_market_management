06321c56632fc8465974413877198c2e977917a1
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-02 16:14:53 -0600

    add contents in transaction viewmodel

--- WebApp/ViewModels/TransactionsViewModel.cs
+++ WebApp/ViewModels/TransactionsViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace WebApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using WebApp.Models;
+
+namespace WebApp.ViewModels
 {
 	public class TransactionsViewModel
 	{
+		[Display(Name = "Cashier Name")]
+		public string? CashierName { get; set; }
+
+		[Display(Name = "Start Date")]
+		public DateTime StartDate { get; set; } = DateTime.Today;
+
+		[Display(Name = "End Date")]
+		public DateTime EndDate { get; set; } = DateTime.Today;
+
+		public IEnumerable<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+
 	}
 }
