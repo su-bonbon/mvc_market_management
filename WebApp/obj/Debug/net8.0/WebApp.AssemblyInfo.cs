b879e500b0446a7cd9ddd8e9c51b4428d267a7c4
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-03 22:04:11 -0600

    modify printarea print format

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3bd988ff143b6b3e361b84823938e4b23ae8989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f8847b71d1a32fbdce16c603a36ee8710b36861")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
