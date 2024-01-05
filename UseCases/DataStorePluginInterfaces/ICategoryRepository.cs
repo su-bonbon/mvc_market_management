ae2eb1fc823d663615e91d07b1f921e02f0ea511
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-05 15:45:26 -0600

    create addcategory use cases

--- UseCases/DataStorePluginInterfaces/ICategoryRepository.cs
+++ UseCases/DataStorePluginInterfaces/ICategoryRepository.cs
@@ -4,6 +4,8 @@ namespace UseCases.DataStorePluginInterfaces
 {
     public interface ICategoryRepository
     {
+        void AddCategory(Category category);
+        void DeleteCategoty(int categoryId);
         IEnumerable<Category> GetCategories();
         Category GetCategoryById(int categoryId);
         void UpdateCategory(int categoryId, Category category);
