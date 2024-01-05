69d8b481d854573a8c4466e41db7f67e1b282a28
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-05 15:07:40 -0600

    change namespace as corebusiness

--- CoreBusiness/Category.cs
+++ CoreBusiness/Category.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace WebApp.Models
+namespace CoreBusiness
 {
     public class Category
     {
--- CoreBusiness/Transaction.cs
+++ CoreBusiness/Transaction.cs
@@ -1,4 +1,4 @@
-﻿namespace WebApp.Models
+﻿namespace CoreBusiness
 {
 	public class Transaction
 	{
