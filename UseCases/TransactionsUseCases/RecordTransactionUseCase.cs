d7a89e0e20005df892de07416f4a1f137d02159b
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-06 22:55:02 -0600

    paste transaction usecase files

---
+++ UseCases/TransactionsUseCases/RecordTransactionUseCase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases
+{
+    public class RecordTransactionUseCase : IRecordTransactionUseCase
+    {
+        private readonly ITransactionRepository transactionRepository;
+        private readonly IProductRepository productRepository;        
+
+        public RecordTransactionUseCase(
+            ITransactionRepository transactionRepository,
+            IProductRepository productRepository)
+        {
+            this.transactionRepository = transactionRepository;
+            this.productRepository = productRepository;            
+        }
+
+        public void Execute(string cashierName, int productId, int qty)
+        {
+            var product = productRepository.GetProductById(productId);
+            if (product != null)
+                transactionRepository.Add(
+                    cashierName, 
+                    productId, 
+                    product.Name, 
+                    product.Price.HasValue?product.Price.Value:0,
+                    product.Quantity.HasValue?product.Quantity.Value:0, qty);
+        }
+    }
+}
