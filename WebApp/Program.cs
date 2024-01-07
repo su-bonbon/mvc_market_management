1d08e2ff53cc6771fc3df015f3a3efc0e27aae91
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-06 18:46:19 -0600

    add build dependencies to initiate usecases

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using Plugins.DataStore.InMemory;
 using System.Net.Mime;
 using System.Text;
+using UseCases.CategoriesUseCases;
+using UseCases.DataStorePluginInterfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSingleton<ICategoryRepository, CategoriesInMemoryRepository>();
+builder.Services.AddTransient<IViewCategoriesUseCase, ViewCategoriesUseCase>();
 var app = builder.Build();
 
 app.UseStaticFiles();
