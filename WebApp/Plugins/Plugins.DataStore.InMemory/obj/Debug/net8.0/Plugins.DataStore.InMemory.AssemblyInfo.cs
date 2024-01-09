aa01337fffacfcc9e3a6d0114ed858fa64b52e74
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-09 16:40:58 -0600

    create a interface for sql

--- WebApp/Plugins/Plugins.DataStore.InMemory/obj/Debug/net8.0/Plugins.DataStore.InMemory.AssemblyInfo.cs
+++ WebApp/Plugins/Plugins.DataStore.InMemory/obj/Debug/net8.0/Plugins.DataStore.InMemory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plugins.DataStore.InMemory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db8a7528beab0e82622b284f1f48def5a118037")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plugins.DataStore.InMemory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plugins.DataStore.InMemory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
