3e49d6709941b2f5778ae39b75583461c947b5a0
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-09 16:45:44 -0600

    implement interface for SQL

--- Plugins/Plugins.DataStore.SQL/CategorySQLRepository.cs
+++ Plugins/Plugins.DataStore.SQL/CategorySQLRepository.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using CoreBusiness;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
 
 namespace Plugins.DataStore.SQL
 {
-    internal class CategorySQLRepository
+    internal class CategorySQLRepository : ICategoryRepository
     {
+        private readonly MarketContext db;
+
+        public CategorySQLRepository(MarketContext db)
+        {
+            this.db = db;
+        }
+        public void AddCategory(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            var category = db.Categories.Find(categoryId);
+            if (category == null) return;
+
+            db.Categories.Remove(category);
+            db.SaveChanges();
+
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return db.Categories.ToList();
+        }
+
+        public Category? GetCategoryById(int categoryId)
+        {
+            return db.Categories.Find(categoryId);
+        }
+
+        public void UpdateCategory(int categoryId, Category category)
+        {
+            if (categoryId != category.CategoryId) return;
+
+            var cat = db.Categories.Find(categoryId);
+            if (cat == null) return;
+
+            cat.Name = category.Name;
+            cat.Description = category.Description;
+            db.SaveChanges();
+        }
     }
 }
