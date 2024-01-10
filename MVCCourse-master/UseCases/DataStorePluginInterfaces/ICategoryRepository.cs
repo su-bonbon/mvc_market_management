54c09e45233343701b7f74b61735d59c58ee2608
Sujin <108903065+su-bonbon@users.noreply.github.com>
2024-01-10 14:26:54 -0600

    resolve the error: cs0570

---
+++ MVCCourse-master/UseCases/DataStorePluginInterfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using CoreBusiness;
+
+namespace UseCases.DataStorePluginInterfaces
+{
+    public interface ICategoryRepository
+    {
+        void AddCategory(Category category);
+        void DeleteCategory(int categoryId);
+        IEnumerable<Category> GetCategories();
+        Category? GetCategoryById(int categoryId);
+        void UpdateCategory(int categoryId, Category category);
+    }
+}
